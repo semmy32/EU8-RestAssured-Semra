c05263f07637b7d0bfe4c737d81e0af1190b073a
semmy32 <semra.kavasgenc@gmail.com>
2022-06-23 20:47:25 +0100

    wikipedia page task

--- cydeo-cucumber-junit/src/test/java/com/cydeo/pages/WikiSearchPage.java
+++ cydeo-cucumber-junit/src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -1,2 +1,27 @@
-package com.cydeo.pages;public class WikiSearchPage {
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class WikiSearchPage {
+    public WikiSearchPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "searchInput")
+    public WebElement searchBox;
+
+    @FindBy(css = ".pure-button")
+    public WebElement searchButton;
+
+    @FindBy(id="firstHeading")
+    public WebElement mainHeader;
+
+
+
+    
+
+    
 }
