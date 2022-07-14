f5b5d9a7654c8a20af6b09115a10537446b3e534
semmy32 <semra.kavasgenc@gmail.com>
2022-07-15 00:11:09 +0100

    first

---
+++ EU8-RestAssured-Semra/src/test/java/com/cydeo/Day1/SimpleGetRequest.java
@@ -0,0 +1,24 @@
+package com.cydeo.Day1;
+
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+   String url="http://44.202.144.172:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.get(url);
+        System.out.println(response.statusCode());
+
+          response.prettyPrint();
+
+    }
+
+
+
+}
