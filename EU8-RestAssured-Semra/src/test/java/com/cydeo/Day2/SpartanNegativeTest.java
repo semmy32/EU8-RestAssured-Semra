3b983c3aea31f1d12e5b62414eb1b25a138418f2
semmy32 <semra.kavasgenc@gmail.com>
2022-07-16 19:58:55 +0100

    Negative test

---
+++ EU8-RestAssured-Semra/src/test/java/com/cydeo/Day2/SpartanNegativeTest.java
@@ -0,0 +1,29 @@
+package com.cydeo.Day2;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SpartanNegativeTest {
+    @BeforeAll
+    public static void init(){
+        RestAssured.baseURI="http://44.202.144.172:8000";
+    }
+
+    @DisplayName("GET request to /api/spartans/10")
+   @Test
+    public void test1(){
+       Response response = given().accept(ContentType.XML).when().get("/api/spartans/10");
+       assertEquals(406,response.statusCode());
+       assertEquals("application/xml;charset=UTF-8",response.contentType());
+
+   }
+
+}
