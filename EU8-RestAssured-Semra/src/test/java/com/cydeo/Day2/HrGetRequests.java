cc076576aebcc69fd6ccb5b03647d8400e185563
semmy32 <semra.kavasgenc@gmail.com>
2022-07-16 17:48:54 +0100

    static import implemented

--- EU8-RestAssured-Semra/src/test/java/com/cydeo/Day2/HrGetRequests.java
+++ EU8-RestAssured-Semra/src/test/java/com/cydeo/Day2/HrGetRequests.java
@@ -1,10 +1,15 @@
 package com.cydeo.Day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class HrGetRequests {
     @BeforeAll
@@ -15,8 +20,26 @@ public class HrGetRequests {
     @DisplayName("GET request to regions")
     @Test
     public void test1(){
-        Response response=RestAssured.get("/regions");
+        Response response=RestAssured.given().accept(ContentType.JSON).when().get("/regions");
         System.out.println(response.statusCode());
+    }
+    @DisplayName("GET request to /regions/2")
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .when()
+                .get("/regions/2");
+
+        //verify status code
+        assertEquals(200,response.statusCode());
+        //verify content type
+        assertEquals("application/json",response.contentType());
+
+        response.prettyPrint();
+
+        //verify body contains Americas
+        assertTrue(response.body().asString().contains("Americas"));
+
     }
 
 
